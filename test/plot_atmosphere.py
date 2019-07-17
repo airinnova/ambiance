5571394a035bb54e9000d70123f5e3064e6a10ea
Aaron Dettmann <dettmann@kth.se>
2019-07-17 16:32:29 +0200

    Updated the README file

--- test/plot_atmosphere.py
+++ test/plot_atmosphere.py
@@ -7,18 +7,13 @@ import matplotlib.pyplot as plt
 from ambiance import Atmosphere
 
 
-def main():
-    # Make an atmosphere object
-    heights = np.linspace(-5e3, 80e3, num=1000)
-    atmos = Atmosphere(heights)
+# Make an atmosphere object
+heights = np.linspace(-5e3, 80e3, num=1000)
+atmosphere = Atmosphere(heights)
 
-    # Make plot
-    plt.plot(atmos.temperature_in_celsius, heights/1000)
-    plt.ylabel('Height [km]')
-    plt.xlabel('Temperature [°C]')
-    plt.grid()
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
+# Make plot
+plt.plot(atmosphere.temperature_in_celsius, heights/1000)
+plt.ylabel('Height [km]')
+plt.xlabel('Temperature [°C]')
+plt.grid()
+plt.show()
